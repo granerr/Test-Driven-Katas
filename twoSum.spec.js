89fabbe82c6273088751bfaf8a4ea63772af3685
Rose Graner <rosegraner@gmail.com>
2019-01-29 11:02:58 -0600

    01292019 start of twoSum testing

---
+++ twoSum.spec.js
@@ -0,0 +1,14 @@
+const { expect } = require('chai');
+const wrap = require('./wrap');
+
+describe('twoSum', () => {
+  it('Returns empty array for empty array', () => {
+    expect(twoSum([])).to.equal([])
+  })
+  // it('Does not use the same indices twice', () => {
+  //   expect(twoSum([1, 2, 3, 4], 5).to.equal([]))
+  // })
+
+
+
+})
